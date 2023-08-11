dafc565447404773255f904a87b2539217c4bf70
SergoDobro <optimgrey@yandex.ru>
2023-08-11 15:24:17 +0300

    changed examples

--- ConsoleApp_For_Git/ICalculator.cs
+++ ConsoleApp_For_Git/ICalculator.cs
@@ -14,15 +14,15 @@ namespace ConsoleApp_For_Git
         ICalculator_Operation FindOperation(string signature);
 
         double SolveEquation(string equation);
-        /// 1 - convertToWorkingData
+        /// convert to Format
         /// save indexes of opening skobkas to list, when you find closing skobkas, решите выражение между индексами и удалите использованные элементы 
 
         List<string> SplitEquationToFormat(string equation);
         ///exmplanation
-        ///turn 1+2+(7*9-(3-2))
-        ///into list {1}{+}{2}{+}{(}{7}{*}{9}{-}{(}{3}{-}{2}{)}{)}
+        ///turn 1+2+(7.123*9-(3-2))
+        ///into list {1}{+}{2}{+}{(}{7.123}{*}{9}{-}{(}{3}{-}{2}{)}{)}
 
-            
-        double solveSkobka(List<string> span, int indexFrom, int indexTo); //Replace old partindata and move all second skobka indexes
+
+        double SolveSkobka(List<string> span, int indexFrom, int indexTo); //Replace old partindata and move all second skobka indexes
     }
 }
