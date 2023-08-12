7e93c7642e0352a531ac4b68c9c346c6e33e299f
SergoDobro <optimgrey@yandex.ru>
2023-08-12 13:58:00 +0300

    almost done

--- ConsoleApp_For_Git/Calculator.cs
+++ ConsoleApp_For_Git/Calculator.cs
@@ -18,41 +18,48 @@ namespace ConsoleApp_For_Git
         }
 
         public void LoadOperationList()
-        {
+        { 
             calculator_Operations = new List<ICalculator_Operation>();
             calculator_Operations.Add(new Operations.Operation_Plus());
             calculator_Operations.Add(new Operations.Operation_Minus());
             calculator_Operations.Add(new Operations.Operation_Multiply());
             calculator_Operations.Add(new Operations.Operation_Divide());
+            calculator_Operations.Add(new Operations.Operation_Power());
         }
 
         public double SolveEquation(string equation)
         {
             var data = SplitEquationToFormat(equation);
             List<int> openningSkobkas = new List<int>();
+            data.Insert(0, "(");
+            data.Add(")");
             for (int i = 0; i < data.Count; i++)
             {
                 if (data[i] == "(")
                     openningSkobkas.Add(i);
                 if (data[i]== ")")
                 {
+                    int from = openningSkobkas.Last();
+                    int to = i;
                     int oldDataL = data.Count;
-                    SolveSkobka(data, openningSkobkas.Last(), i);
+                    var _res=SolveSkobka(data, from, to);
                     int newDataL = data.Count;
-                    i -= (oldDataL - newDataL);
+                    i -= (from - to);
                     //1-2+ (1-3)
                     //1-2+ {-2}
                     //1234 56789
                     //1234 5
+                    ReplaceRange(data, from, to, _res.ToString());
                 }
-            }
-            data.Insert(0, "(");
-            data.Add(")");
-            SolveSkobka(data, 0, data.Count-1);
+            } 
             double res = double.Parse(data[0]);
             return res;
         }
-
+        private void ReplaceRange(List<string> span, int from, int to, string item)
+        {
+            span.RemoveRange(from+1, to);
+            span[from] = item;
+        }
         public double SolveSkobka(List<string> span, int indexFrom, int indexTo)
         {
             List<string> primer = span.GetRange(indexFrom+1, indexTo-1);
@@ -89,7 +96,7 @@ namespace ConsoleApp_For_Git
             string nums = "0123456789.";
             for (int i = 1; i < equation.Length; i++)
             {
-                if (nums.Contains(equation[i])&&flag)
+                if (nums.Contains(equation[i]) && flag)
                 {
                     a += equation[i];
                 }
@@ -98,25 +105,28 @@ namespace ConsoleApp_For_Git
                     formatString.Add(a);
                     a = "";
                     flag = true;
+                    i--;
                 }
                 else if (!nums.Contains(equation[i]) && flag)
                 {
                     formatString.Add(a);
                     a = "";
                     flag = false;
+                    i--;
                 }
                 else
                 {
                     a += equation[i];
                 }
 
-
             }
+            formatString.Add(a);
             return formatString;
         }
 
         public void Start()
         {
+            LoadOperationList();
             Console.WriteLine("Write your expression:");
             string expression = Console.ReadLine();
             double result = SolveEquation(expression);
